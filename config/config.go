f839c588ca4db5cf78ff72fde6d431539f836d60
Maximilian <m@ximilian.info>
2018-10-09 12:31:25 +0200

    fix(all): added license header

--- config/config.go
+++ config/config.go
@@ -1,46 +1,58 @@
 package config
 
-import (
-    "github.com/BurntSushi/toml"
-)
+// gitdeploy
+// Copyright (C) 2016 Maximilian Pachl
 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import (
+	"github.com/BurntSushi/toml"
+)
 
 // ----------------------------------------------------------------------------------
 //  constants
 // ----------------------------------------------------------------------------------
 
 const (
-    PROVIDER_GIT = "git"
+	PROVIDER_GIT = "git"
 )
-
 
 // ----------------------------------------------------------------------------------
 //  types
 // ----------------------------------------------------------------------------------
 
 type Config struct {
-    Path string
-    Provider string
+	Path     string
+	Provider string
 
-    Url string `toml:"url"`
-    IdentityFile string `toml:"identity_file"`
-    Branch string `toml:"branch"`
+	Url          string `toml:"url"`
+	IdentityFile string `toml:"identity_file"`
+	Branch       string `toml:"branch"`
 }
-
 
 // ----------------------------------------------------------------------------------
 //  functions
 // ----------------------------------------------------------------------------------
 
 func Load(path string) (*Config, error) {
-    // decode the config file to struct
-    var config Config
-    if _, err := toml.DecodeFile(path, &config); err != nil {
-        return nil, err
-    }
+	// decode the config file to struct
+	var config Config
+	if _, err := toml.DecodeFile(path, &config); err != nil {
+		return nil, err
+	}
 
-    config.Path = path
+	config.Path = path
 
-    return &config, nil
+	return &config, nil
 }
-
--- git/git.go
+++ git/git.go
@@ -1,103 +1,116 @@
 package git
 
-import (
-    "os"
-    "os/exec"
-    "path/filepath"
-)
+// gitdeploy
+// Copyright (C) 2016 Maximilian Pachl
 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import (
+	"os"
+	"os/exec"
+	"path/filepath"
+)
 
 // ----------------------------------------------------------------------------------
 //  constants
 // ----------------------------------------------------------------------------------
 
 const (
-    GIT_DIRECTORY = ".git"
-    GIT_BINARY = "git"
-    GIT_REMOTE = "origin"
+	GIT_DIRECTORY = ".git"
+	GIT_BINARY    = "git"
+	GIT_REMOTE    = "origin"
 
-    SSH_BINARY = "ssh"
+	SSH_BINARY = "ssh"
 
-    UP_TO_DATE = "Already up-to-date."
+	UP_TO_DATE = "Already up-to-date."
 )
-
 
 // ----------------------------------------------------------------------------------
 //  public functions
 // ----------------------------------------------------------------------------------
 
-func IsRepository(dir string) (bool) {
-    path := filepath.Join(dir, GIT_DIRECTORY)
-    _, err := os.Stat(path)
+func IsRepository(dir string) bool {
+	path := filepath.Join(dir, GIT_DIRECTORY)
+	_, err := os.Stat(path)
 
-    return !os.IsNotExist(err)
+	return !os.IsNotExist(err)
 }
 
-func IsInstalled() (bool) {
-    git, err := exec.LookPath(GIT_BINARY)
-    if err != nil {
-        return false
-    }
+func IsInstalled() bool {
+	git, err := exec.LookPath(GIT_BINARY)
+	if err != nil {
+		return false
+	}
 
-    ssh, err := exec.LookPath(SSH_BINARY)
-    if err != nil {
-        return false
-    }
+	ssh, err := exec.LookPath(SSH_BINARY)
+	if err != nil {
+		return false
+	}
 
-    return len(git) > 0 && len(ssh) > 0
+	return len(git) > 0 && len(ssh) > 0
 }
 
 func Clone(path string, url string, identity string, branch string) (string, error) {
-    // initialize the repository
-    out, err := execGit(path, identity, "init")
-    if err != nil {
-        return out, err
-    }
+	// initialize the repository
+	out, err := execGit(path, identity, "init")
+	if err != nil {
+		return out, err
+	}
 
-    // add the url as a new remote
-    out, err = execGit(path, identity, "remote", "add", GIT_REMOTE, url)
-    if err != nil {
-        return out, err
-    }
+	// add the url as a new remote
+	out, err = execGit(path, identity, "remote", "add", GIT_REMOTE, url)
+	if err != nil {
+		return out, err
+	}
 
-    // pull the master branch
-    out, err = execGit(path, identity, "fetch")
-    if err != nil {
-        return out, err
-    }
+	// pull the master branch
+	out, err = execGit(path, identity, "fetch")
+	if err != nil {
+		return out, err
+	}
 
-    // setup upstream brunch
-    return execGit(path, identity, "checkout", "-t", GIT_REMOTE + "/" + branch)
+	// setup upstream brunch
+	return execGit(path, identity, "checkout", "-t", GIT_REMOTE+"/"+branch)
 }
 
 func Pull(path string, identity string) (string, error) {
-    return execGit(path, identity, "pull")
+	return execGit(path, identity, "pull")
 }
-
 
 // ----------------------------------------------------------------------------------
 //  private functions
 // ----------------------------------------------------------------------------------
 
 func execGit(path string, identity string, args ...string) (string, error) {
-    // find the absolute path to the
-    // git cli bianry
-    binary, err := exec.LookPath(GIT_BINARY)
-    if err != nil {
-        return "", err
-    }
+	// find the absolute path to the
+	// git cli bianry
+	binary, err := exec.LookPath(GIT_BINARY)
+	if err != nil {
+		return "", err
+	}
 
-    // build the git command
-    cmd := exec.Command(binary, args...)
-    cmd.Dir = path
-    cmd.Env = []string{"GIT_SSH_COMMAND=" + SSH_BINARY + " " + 
-                       "-F /dev/null " +
-                       "-o UserKnownHostsFile=/dev/null " + 
-                       "-o StrictHostKeyChecking=no " + 
-                       "-i " + identity}
+	// build the git command
+	cmd := exec.Command(binary, args...)
+	cmd.Dir = path
+	cmd.Env = []string{"GIT_SSH_COMMAND=" + SSH_BINARY + " " +
+		"-F /dev/null " +
+		"-o UserKnownHostsFile=/dev/null " +
+		"-o StrictHostKeyChecking=no " +
+		"-i " + identity}
 
-    // get the stdout/stderr of the git process
-    // CombinedOutput() will block until end
-    out, err := cmd.CombinedOutput()
-    return string(out), err
+	// get the stdout/stderr of the git process
+	// CombinedOutput() will block until end
+	out, err := cmd.CombinedOutput()
+	return string(out), err
 }
